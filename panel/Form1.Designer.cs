f26ecaf054abd499d3c847cde718a13d2a1a7716
piyanut.rk <piyanut.rk@gmail.com>
2021-09-26 18:04:58 +0700

    Add project files.

---
+++ panel/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿
+namespace panel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.roundPanel1 = new panel.RoundPanel();
+            this.SuspendLayout();
+            // 
+            // roundPanel1
+            // 
+            this.roundPanel1.Fill_Type = panel.FillType.FillRoundedRectangle;
+            this.roundPanel1.Fill_X = 20;
+            this.roundPanel1.Fill_Y = 20;
+            this.roundPanel1.FillColor = System.Drawing.Color.Blue;
+            this.roundPanel1.FillLineColor = System.Drawing.Color.Blue;
+            this.roundPanel1.FillLineWidth = 10F;
+            this.roundPanel1.FillRadius = 10;
+            this.roundPanel1.Location = new System.Drawing.Point(105, 32);
+            this.roundPanel1.Name = "roundPanel1";
+            this.roundPanel1.Size = new System.Drawing.Size(580, 407);
+            this.roundPanel1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(779, 501);
+            this.Controls.Add(this.roundPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private RoundPanel roundPanel1;
+    }
+}
+
---
+++ panel/RoundPanel.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace panel
+{
+    public class RoundPanel : Panel
+    {
+
+        private Color _FillColor = Color.White;
+        private Color _fillLineColor = Color.Black;
+
+        private int _radius = 10;
+        private int _fill_X = 6;
+        private int _fill_Y = 6;
+        private FillType _fillType = FillType.DrawRoundedRectangle;
+        private float _fillLineWidth = 2.0f;
+
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+            //g.FillRoundedRectangle(new SolidBrush(Color.White), 10, 10, 10, 10, 10);
+            //g.FillRoundedRectangle(new SolidBrush(Color.White), 10, 10, this.Width - 40, this.Height - 60, 10);
+            //SolidBrush brush = new SolidBrush(
+            //    Color.White
+            //    );
+            //g.FillRoundedRectangle(brush, 12, 12, this.Width - 44, this.Height - 64, 10);
+            //g.DrawRoundedRectangle(new Pen(ControlPaint.Light(Color.Red, 0.00f)), 6, 6, this.Width-12, this.Height-12, 10);
+            //g.DrawRoundedRectangle(new Pen(ControlPaint.Light(Color.White, 0.00f)), 12, 12, this.Width - 44, this.Height - 64, 10);            
+            //g.FillRoundedRectangle(new SolidBrush(Color.White), 12, 12 + ((this.Height - 64) / 2), this.Width - 44, (this.Height - 64) / 2, 10);
+
+            switch (_fillType)
+            {
+                case FillType.FillRoundedRectangle:
+                    g.FillRoundedRectangle(new SolidBrush(_FillColor), _fill_X, _fill_Y,
+                        this.Width - (_fill_X * 2), this.Height - (_fill_Y * 2), _radius);
+                    break;
+                case FillType.DrawRoundedRectangle:
+                    g.DrawRoundedRectangle(new Pen(ControlPaint.Light(_fillLineColor, 0.00f), _fillLineWidth),
+                        _fill_X, _fill_Y, this.Width - (_fill_X * 2), this.Height - (_fill_Y * 2), _radius);
+                    g.FillRoundedRectangle(new SolidBrush(_FillColor), _fill_X + _fillLineWidth / 2, _fill_Y + _fillLineWidth / 2,
+                        this.Width - ((_fill_X + _fillLineWidth / 2) * 2), this.Height - ((_fill_Y + _fillLineWidth / 2) * 2),
+                        _radius - _fillLineWidth / 2);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+
+        public float FillLineWidth
+        {
+            get { return _fillLineWidth; }
+            set
+            {
+                _fillLineWidth = value;
+                Invalidate();
+            }
+        }
+
+
+        public Color FillLineColor
+        {
+            get { return _fillLineColor; }
+            set
+            {
+                _fillLineColor = value;
+                Invalidate();
+            }
+        }
+
+
+
+        public Color FillColor
+        {
+            get => _FillColor;
+            set
+            {
+                _FillColor = value;
+                Invalidate();
+            }
+        }
+
+        public int FillRadius
+        {
+            get { return _radius; }
+            set
+            {
+                _radius = value;
+                Invalidate();
+            }
+        }
+
+        public int Fill_X
+        {
+            get { return _fill_X; }
+            set
+            {
+                _fill_X = value;
+                Invalidate();
+            }
+        }
+
+        public int Fill_Y
+        {
+            get { return _fill_Y; }
+            set
+            {
+                _fill_Y = value;
+                Invalidate();
+            }
+        }
+
+        public FillType Fill_Type
+        {
+            get { return _fillType; }
+            set
+            {
+                _fillType = value;
+                Invalidate();
+            }
+        }
+
+
+    }
+
+    public enum FillType { FillRoundedRectangle, DrawRoundedRectangle };
+
+}
